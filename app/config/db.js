bc4294665d2cc77b9e5340916197ed082ae11af3
Lucas Heintz <lucas.heintz@gmail.com>
2019-06-27 12:04:05 +0200

    fix test

--- app/config/db.js
+++ app/config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+console.log(process.env.PASS)
 mongoose.connect(`mongodb+srv://lucas:${process.env.PASS}@freecollab-k0wpo.mongodb.net/free-collab`, {useNewUrlParser: true, dbName: "free-collab"})
 
 // Connection a la DB
